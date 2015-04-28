34b51ccc8d96abc85e3039d0e7ffab1c7ffbab87
Matt Crider <mattcrider@gmail.com>
2015-04-28 17:14:24 -0400

    Added gulp-awspublish; use with `gulp deploy`

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var util = require('gulp-util');
 var notifier = require('node-notifier');
 var neat = require('node-neat').includePaths;
 var debowerify = require("debowerify");
+var fs = require("fs");
+var awspublish = require('gulp-awspublish');
 
 
 var paths = {
@@ -78,6 +80,33 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('scripts-watch', ['scripts'], browserSync.reload);
+
+gulp.task('deploy', function() {
+
+  // create a new publisher using S3 options
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
+  credentials = JSON.parse(fs.readFileSync('./aws.json'));
+  var publisher = awspublish.create(credentials);
+
+  // define custom headers
+  var headers = {
+    'Cache-Control': 'max-age=315360000, no-transform, public'
+  };
+
+  return gulp.src('./build/**')
+     // gzip, Set Content-Encoding headers and add .gz extension
+    .pipe(awspublish.gzip({ ext: '.gz' }))
+
+    // publisher will add Content-Length, Content-Type and headers specified above
+    // If not specified it will set x-amz-acl to public-read by default
+    .pipe(publisher.publish(headers))
+
+    // create a cache file to speed up consecutive uploads
+    .pipe(publisher.cache())
+
+     // print upload updates to console
+    .pipe(awspublish.reporter());
+});
 
 gulp.task('browser-sync', function(){
   browserSync({
